3efc6c19f276776150d67825200060a802c3eaf1
Ying Wang <yingw787.personal@gmail.com>
2024-02-14 15:18:29 -0500

    Update schema.ts adding Mutation and mutation response types

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -52,4 +52,19 @@ export const typeDefs = gql`
     "The module's video url, for video-based modules"
     videoUrl: String
   }
+
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
+  }
 `;
